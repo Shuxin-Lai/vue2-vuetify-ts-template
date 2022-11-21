6691f721348f8ed220c72bc7ccf32d164c0db87d
Laishuxin <15622971239@163.com>
2022-11-21 23:31:23 +0800

    chore: router

--- src/Routes.ts
+++ src/Routes.ts
@@ -11,8 +11,8 @@ import Notifications from '@/pages/Notifications/Notifications.vue'
 import Icons from '@/pages/Icons/Icons.vue'
 import Charts from '@/pages/Charts/Charts.vue'
 import Maps from '@/pages/Maps/Google.vue'
-import Error from '@/pages/Error/Error.vue'
-import Login from '@/pages/Login/Login.vue'
+// import Error from '@/pages/Error/Error.vue'
+// import Login from '@/pages/Login/Login.vue'
 
 Vue.use(Router)
 
@@ -21,55 +21,55 @@ const router = new Router({
     {
       path: '/login',
       name: 'Login',
-      component: Login,
+      component: () => import('@/pages/Login/Login.vue'),
     },
     {
       path: '/',
       name: 'Layout',
       redirect: 'dashboard',
-      component: Layout,
+      component: () => import('@/components/Layout/Layout.vue'),
       children: [
         {
           path: 'dashboard',
           name: 'Dashboard',
-          component: Dashboard,
+          component: () => import('@/pages/Dashboard/Dashboard.vue'),
         },
         {
           path: 'typography',
           name: 'Typography',
-          component: Typography,
+          component: () => import('@/pages/Typography/Typography.vue'),
         },
         {
           path: 'tables',
           name: 'Tables',
-          component: Tables,
+          component: () => import('@/pages/Tables/Basic.vue'),
         },
         {
           path: 'notifications',
           name: 'Notifications',
-          component: Notifications,
+          component: () => import('@/pages/Notifications/Notifications.vue'),
         },
         {
           path: 'icons',
           name: 'Icons',
-          component: Icons,
+          component: () => import('@/pages/Icons/Icons.vue'),
         },
         {
           path: 'charts',
           name: 'Charts',
-          component: Charts,
+          component: () => import('@/pages/Charts/Charts.vue'),
         },
-        {
-          path: 'maps',
-          name: 'Maps',
-          component: Maps,
-        },
+        // {
+        //   path: 'maps',
+        //   name: 'Maps',
+        //   component: Maps,
+        // },
       ],
     },
     {
       path: '*',
       name: 'Error',
-      component: Error,
+      component: () => import('@/pages/Error/Error.vue'),
     },
   ],
   mode: 'history',
